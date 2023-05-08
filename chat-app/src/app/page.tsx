bc103938ca4c99f43c3296f9d39ed1cb54c005fe
Danilo Leão <olin_ad@hotmail.com>
2023-05-08 09:20:27 -0300

    feat: initial setup with tailwind and bunch of libs

---
+++ chat-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Button from "./ui/Button";
+
+export default function Home() {
+  return (
+    <main className="text-red-500">
+      <Button>
+        click here
+      </Button>
+    </main>
+  )
+}
